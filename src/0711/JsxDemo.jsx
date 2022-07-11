24ce477e5fe370c5b02f30cd04e39df973470f92
hunter850 <91997043+hunter850@users.noreply.github.com>
2022-07-11 10:22:18 +0800

    hooks prettier

--- src/0711/JsxDemo.jsx
+++ src/0711/JsxDemo.jsx
@@ -3,7 +3,27 @@ import { Fragment } from "react";
 function JsxDemo() {
     return (
         <Fragment>
-            <h1>Hello JsxDemo</h1>
+            <h1>Number</h1>
+            {152 - 1}
+            {NaN}
+            <hr />
+            <h1>String</h1>
+            {"Hello React"}
+            {`Hello JSX`}
+            <hr />
+            <h1>Boolean</h1>
+            {true}
+            {false}
+            <hr />
+            <h1>null</h1>
+            {null}
+            <hr />
+            <h1>undefined</h1>
+            {undefined}
+            <hr />
+            <h1>Array</h1>
+            {[1, 2, 3, 4]}
+            <hr />
         </Fragment>
     );
 }
--- src/hooks/useIndexArray.jsx
+++ src/hooks/useIndexArray.jsx
@@ -4,17 +4,17 @@ import { v4 } from "uuid";
 const useIndexArray = (num, ...items) => {
     const output = useMemo(() => {
         const tempArray = [];
-        for(let i = 0; i < num; i++) {
-            const tempObj = {index: i};
+        for (let i = 0; i < num; i++) {
+            const tempObj = { index: i };
             items.forEach((item, index) => {
                 tempObj[`content${index}`] = item + "" + i;
-            })
+            });
             tempObj.id = v4();
             tempArray.push(tempObj);
         }
         return tempArray;
     }, [num, items]);
     return output;
-}
+};
 
-export default useIndexArray
+export default useIndexArray;
