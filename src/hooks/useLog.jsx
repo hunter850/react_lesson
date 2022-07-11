5a9cdc4c6c220c2eb93e7215db009635c52420ee
hunter850 <91997043+hunter850@users.noreply.github.com>
2022-07-11 12:25:29 +0800

    useLog 忽略value的dependency

--- src/hooks/useLog.jsx
+++ src/hooks/useLog.jsx
@@ -3,6 +3,7 @@ import { useEffect } from "react";
 const useLog = (...value) => {
     useEffect(() => {
         console.log(...value);
+        // eslint-disable-next-line react-hooks/exhaustive-deps
     }, value);
 };
 
