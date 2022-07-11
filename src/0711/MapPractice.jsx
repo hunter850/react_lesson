72be457e7ab974a9723ace2ec12879d3886bae85
hunter850 <91997043+hunter850@users.noreply.github.com>
2022-07-11 12:55:46 +0800

    add container

--- src/0711/MapPractice.jsx
+++ src/0711/MapPractice.jsx
@@ -1,36 +1,39 @@
 import { Fragment } from "react";
+import { Container } from "react-bootstrap";
 import data from "./data/products.json";
 import "./css/MapDemo.css";
 
 function MapPractice() {
     return (
         <Fragment>
-            <table>
-                <thead>
-                    <tr>
-                        <th>id</th>
-                        <th>picture</th>
-                        <th>stock</th>
-                        <th>name</th>
-                        <th>price</th>
-                        <th>tags</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {data.map((item) => (
-                        <tr key={item.id}>
-                            <td>{item.id}</td>
-                            <td>
-                                <img src={item.picture} alt={item.name} />
-                            </td>
-                            <td>{item.stock}</td>
-                            <td>{item.name}</td>
-                            <td>{item.price}</td>
-                            <td>{item.tags}</td>
+            <Container className="mx-auto">
+                <table>
+                    <thead>
+                        <tr>
+                            <th>id</th>
+                            <th>picture</th>
+                            <th>stock</th>
+                            <th>name</th>
+                            <th>price</th>
+                            <th>tags</th>
                         </tr>
-                    ))}
-                </tbody>
-            </table>
+                    </thead>
+                    <tbody>
+                        {data.map((item) => (
+                            <tr key={item.id}>
+                                <td>{item.id}</td>
+                                <td>
+                                    <img src={item.picture} alt={item.name} />
+                                </td>
+                                <td>{item.stock}</td>
+                                <td>{item.name}</td>
+                                <td>{item.price}</td>
+                                <td>{item.tags}</td>
+                            </tr>
+                        ))}
+                    </tbody>
+                </table>
+            </Container>
         </Fragment>
     );
 }
